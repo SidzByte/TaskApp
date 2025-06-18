adc999fbd40653511a62790dbb109a476d48d017
SidzByte <sidz007x@gmail.com>
2025-06-18 09:30:03 +0530

    first commit

--- app/src/main/java/com/example/taskapp/MainActivity.java
+++ app/src/main/java/com/example/taskapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.taskapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.taskapp.utils.DataBaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-    }
-}
+//        private RecyclerView mRecyclerview;
+//        private FloatingActionButton fab;
+//        private DataBaseHelper myDB;
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_main);
+//
+//            mRecyclerview = findViewById(R.id.recyclerview);
+//            fab = findViewById(R.id.fab);
+//            myDB = new DataBaseHelper(MainActivity.this);
+//
+//            fab.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                }
+//
+//            });
+//    }
+//}
